ec8e41cda69c76f58d9c222f78a667d1f3b81095
Janne Pakarinen <janne.pakarinen@documill.com>
2015-05-29 16:43:32 +0300

    Eh, fixed slightly, more to do

--- tetro.c
+++ tetro.c
@@ -11,6 +11,7 @@ const char TetroCharacters[] = { '\0', 'I', 'L', 'J', 'Z', 'S', 'O', 'T' };
 
 // Private headers
 
+static int tetro_rotate_left_int(int v);
 /** Copies shape from in to out. */
 static void tetro_shape_copy(char from[16],Tetro *tetro);
 /** Sets all values in shape to null character. */
@@ -18,6 +19,14 @@ static void tetro_shape_null(Tetro *tetro);
 
 // Implementations
 // Private
+static int tetro_rotate_left_int(int v)
+{
+  int x,y;
+  x = v % 4;
+  y = (v - x) / 4;
+
+
+}
 
 static void tetro_shape_copy(char from[16],Tetro *tetro)
 {
@@ -47,46 +56,46 @@ Tetro tetro_init(TetroType tetro_type)
   switch (tetro_type)
   {
   case TETRO_I:
-    tetro.shape[1][0] = ch; // 1,0
-    tetro.shape[1][1] = ch;
-    tetro.shape[1][2] = ch;
-    tetro.shape[1][3] = ch;
+    tetro.shape[2] = ch; // 1,0
+    tetro.shape[5] = ch; // 1,1
+    tetro.shape[9] = ch; // 1,2
+    tetro.shape[13] = ch; // 1,3
     break;
   case TETRO_L:
-    tetro.shape[1][0] = ch;
-    tetro.shape[1][1] = ch;
-    tetro.shape[1][2] = ch;
-    tetro.shape[0][2] = ch;
+    tetro.shape[2] = ch; // 1,0
+    tetro.shape[5] = ch; // 1,1
+    tetro.shape[9] = ch; // 1,2
+    tetro.shape[3] = ch; // 0,2
     break;
   case TETRO_J:
-    tetro.shape[1][0] = ch;
-    tetro.shape[1][1] = ch;
-    tetro.shape[1][2] = ch;
-    tetro.shape[0][0] = ch;
+    tetro.shape[2] = ch; // 1,0
+    tetro.shape[5] = ch; // 1,1
+    tetro.shape[9] = ch; // 1,2
+    tetro.shape[0] = ch; // 0,0
     break;
   case TETRO_Z:
-    tetro.shape[0][0] = ch;
-    tetro.shape[0][1] = ch;
-    tetro.shape[1][1] = ch;
-    tetro.shape[1][2] = ch;
+    tetro.shape[0] = ch; // 0,0
+    tetro.shape[1] = ch; // 0,1
+    tetro.shape[5] = ch; // 1,1
+    tetro.shape[6] = ch; // 1,2
     break;
   case TETRO_S:
-    tetro.shape[0][1] = ch;
-    tetro.shape[0][2] = ch;
-    tetro.shape[1][0] = ch;
-    tetro.shape[1][1] = ch;
+    tetro.shape[1] = ch; // 0,1
+    tetro.shape[2] = ch; // 0,2
+    tetro.shape[4] = ch; // 1,0
+    tetro.shape[5] = ch; // 1,1
     break;
   case TETRO_O:
-    tetro.shape[0][1] = ch;
-    tetro.shape[0][2] = ch;
-    tetro.shape[1][1] = ch;
-    tetro.shape[1][2] = ch;
+    tetro.shape[1] = ch; // 0,1
+    tetro.shape[2] = ch; // 0,2
+    tetro.shape[5] = ch; // 1,1
+    tetro.shape[6] = ch; // 1,2
     break;
   case TETRO_T:
-    tetro.shape[0][1] = ch;
-    tetro.shape[1][0] = ch;
-    tetro.shape[1][1] = ch;
-    tetro.shape[1][2] = ch;
+    tetro.shape[1] = ch; // 0,1
+    tetro.shape[4] = ch; // 1,0
+    tetro.shape[5] = ch; // 1,1
+    tetro.shape[6] = ch; // 1,2
     break;
   default:
     exit(EXIT_FAILURE);
@@ -108,17 +117,16 @@ void tetro_turn_clockwise(Tetro *tetro)
   if (_tetro.type > 0 && _tetro.type < TETRO_TYPE_MAX &&
       _tetro.type != TETRO_O)
   {
-    int i,j;
-    char old_shape[4][4];
-    char new_shape[4][4];
+    int i;
+    char old_shape[16];
+    char new_shape[16];
 
     tetro_shape_copy(_tetro.shape,&old_shape);
     switch (_tetro.type)
     {
     case TETRO_I:
       for (i = 0; i < 4; i++)
-        for (j = 0; j < 4; j++)
-          new_shape[i][j] = old_shape[3-j][i];
+        new_shape[i] = old_shape[3-i];
       break;
     case TETRO_L:
     case TETRO_J:
